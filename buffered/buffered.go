be1593ddebe0a93d146b0df2e5e0bc392dc90273
Alex Drummond <a.d.drummond@gmail.com>
2019-04-19 12:31:13 +0100

    still some errors for buffered

--- buffered/buffered.go
+++ buffered/buffered.go
@@ -26,7 +26,7 @@ type BufferedISkipList struct {
 
 // If a slice is no longer than this, then we perform all operations directly on
 // the slice when possible.
-const noHoldsBarredMaxLength = 128
+const noHoldsBarredMaxLength = 32
 
 // We don't let either 'start' or 'end' grow longer than maxSliceLength.
 // This is to prevent counterintuitive performance characteristics. For example,
@@ -39,7 +39,7 @@ const noHoldsBarredMaxLength = 128
 // 'start' or 'end' grow too big. This doesn't increase aggregate performance,
 // but makes the performance characteristics of individual operations more
 // predictable.
-const maxSliceLength = 256
+const maxSliceLength = 64
 
 func checkStartSliceGrowth(l *BufferedISkipList) {
 	if len(l.start) >= maxSliceLength {
@@ -52,6 +52,7 @@ func checkStartSliceGrowth(l *BufferedISkipList) {
 
 func checkEndSliceGrowth(l *BufferedISkipList) {
 	if len(l.end) >= maxSliceLength {
+		panic("HERE1")
 		for _, v := range l.end {
 			l.iskiplist.PushBack(v)
 		}
